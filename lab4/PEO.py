847dae64610c27547139c352636d2033923987f7
agga1 <agud100@gmail.com>
2019-12-02 23:57:02 +0100

    max_clique working

--- lab4/PEO.py
+++ lab4/PEO.py
@@ -1,3 +1,7 @@
+import os
+
+from utils.create_graph import create_graph
+from lab4.lexBFS import lexBFS
 from lab4.Node import Node
 from typing import List
 
@@ -20,3 +24,17 @@ def is_PEO(G: List[Node], ord: List[int]) -> bool:
         if len(diff) > 0:
             return False
     return True
+
+
+# graphs = []
+# for name in os.listdir("res/chordal"):
+#     graphs.append(f"res/chordal/{name}")
+#
+# graphs = ["res/chordal/cycle4"]
+# for ind, graph in enumerate(graphs):
+#     if ind > 0:
+#         break
+#     G = create_graph(graph)
+#     vs = lexBFS(G)
+#     graph = graph.split("/")[-1]
+#     print(is_PEO(G, vs))
---
+++ lab4/max_clique.py
@@ -0,0 +1,36 @@
+from typing import List
+from lab4.Node import Node
+from lab4.is_chordal import is_chordal
+from utils.test_utils import test
+from utils.test_utils import load_graph_as_nodes
+from lab4.lexBFS import lexBFS
+
+
+@test(data="res/maxclique", loader=load_graph_as_nodes)
+def biggest_clique(G: List[Node]) -> int:
+    """ returns size of the biggest clique in given chordal graph """
+    assert (is_chordal(G)), "Received graph is not chordal! "
+    ord = lexBFS(G)
+    max_size = 0
+    RN = [set() for _ in range(len(G))]  # RN[v] -  zbiór sąsiadów pojawiających się wcześniej niż v
+    for v in ord:
+        for el in ord:
+            if el == v:
+                break
+            if el in G[v].out:
+                RN[v].add(el)
+        max_size = max(max_size, len(RN[v])+1)
+
+    return max_size
+
+
+# graphs = []
+# for name in os.listdir("res/maxclique"):
+#     graphs.append(f"res/maxclique/{name}")
+# # graphs = ["res/chordal/cycle4"]
+# for ind, graph in enumerate(graphs):
+#     # if ind > 0:
+#     #     break
+#     G = create_graph(graph)
+#     graph = graph.split("/")[-1]
+#     print(biggest_clique(G), " for ", graph)
